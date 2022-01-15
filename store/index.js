3a776822bd349636a665b18c5c264ba14518d3be
pete <32456803+kasia007@users.noreply.github.com>
2022-01-15 15:50:46 +0900

    Update index.js

--- store/index.js
+++ store/index.js
@@ -1,12 +1,9 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import { createStore } from 'vuex'
 
 import { module as common } from './modules/common/commonStore'
 import { module as main } from './modules/main/mainStore'
 
-Vue.use(Vuex)
-
-export default new Vuex.Store({
+export default createStore({
   modules: {
     common,
     main
